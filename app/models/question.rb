6432b0d0dd6f0c37516c440fbb3e4ee827b77771
Anshika Agarwal <anshika2020ag@gmail.com>
2026-01-27 14:26:20 +0530

    question can not be edited if it has ans/comment/vote

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,15 +1,18 @@
 class Question < ApplicationRecord
     enum :status, { draft: 0, published: 1, unpublished: 2 }
 
+    # callback
+    before_update :check_if_editable?
+
     # associations
     belongs_to :user
 
     has_many :topic_assignements, as: :topicable
     has_many :topics, through: :topic_assignements, optional: false
 
-    has_many :answers,  dependent: :destroy
-    has_many :comments, dependent: :destroy, as: :commentable
-    has_many :votes,    dependent: :destroy, as: :votable
+    has_many :answers,  dependent: :restrict_with_error
+    has_many :comments, dependent: :restrict_with_error, as: :commentable
+    has_many :votes,    dependent: :restrict_with_error, as: :votable
 
     has_many :abuse_reports, as: :reportable, dependent: :destroy
 
@@ -22,6 +25,13 @@ class Question < ApplicationRecord
     validate  :atleast_1_credit_needed, on: :create
     validate  :must_have_topics_if_published
     validate  :pdf_type
+
+    private def check_if_editable
+        return unless answers.exists? || comments.exists? || votes.exists?
+
+        errors.add(:base, "Question can not be edited if answers/comments/votes are present")
+        throw(:abort)
+    end
 
     private def atleast_1_credit_needed
         if user.credits < 1
