3d54566c24d0fe01d5d632eb606fa9df29ef0d15
Anshika Agarwal <anshika2020ag@gmail.com>
2026-01-29 15:38:28 +0530

    default avatar of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
   # callbacks
   before_create :set_defaults_for_admins
+  after_commit :attach_default_avatar, on: [ :create ]
 
   # associations
   has_many :questions, dependent: :destroy
@@ -74,5 +75,15 @@ class User < ApplicationRecord
 
   def admin?
     role == "admin"
+  end
+
+  def attach_default_avatar
+    return if avatar.attached?
+
+    avatar.attach(
+      io: File.open(Rails.root.join("app/assets/images/placeholder_user_avatar.png")),
+      filename: "placeholder_user_avatar.png",
+      content_type: "image/png"
+    )
   end
 end
