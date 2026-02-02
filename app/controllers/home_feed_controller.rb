048ee86be9792bc16f51c9d110dfbb06908f9597
Anshika Agarwal <anshika2020ag@gmail.com>
2026-02-02 15:15:15 +0530

    added draft status to views

--- app/controllers/home_feed_controller.rb
+++ app/controllers/home_feed_controller.rb
@@ -2,7 +2,7 @@ class HomeFeedController < ApplicationController
     before_action :set_current_user
 
     def index
-        @questions = Question.all
+        @questions = Question.published
     end
 
     private def set_current_user
