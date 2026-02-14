70755b5c1cf4209ba9b35e3ed03cef2847955f3f
Anshika Agarwal <anshika2020ag@gmail.com>
2026-02-14 12:20:45 +0530

    fixed indentation in controllers

--- app/controllers/home_feed_controller.rb
+++ app/controllers/home_feed_controller.rb
@@ -1,23 +1,23 @@
 class HomeFeedController < ApplicationController
-    before_action :set_current_user
+  before_action :set_current_user
 
-    def index
-      if params[:query]
-        term = params[:query][:content].to_s.strip
+  def index
+    if params[:query]
+      term = params[:query][:content].to_s.strip
 
-        if params[:query][:search_by] == "topic"
-          topic_filter = Topic.sanitize_sql_like(term) + "%"
-          @questions = Question.where.associated(:topics).and(Topic.where("name ILIKE ?", topic_filter))
-        else
-          content_filter = Question.sanitize_sql_like(term) + "%"
-          @questions = Question.published.where("title ILIKE ?", content_filter)
-        end
+      if params[:query][:search_by] == "topic"
+        topic_filter = Topic.sanitize_sql_like(term) + "%"
+        @questions = Question.where.associated(:topics).and(Topic.where("name ILIKE ?", topic_filter))
       else
-        @questions = Question.published.order(created_at: :desc)
+        content_filter = Question.sanitize_sql_like(term) + "%"
+        @questions = Question.published.where("title ILIKE ?", content_filter)
       end
+    else
+      @questions = Question.published.order(created_at: :desc)
     end
+  end
 
-    private def set_current_user
-        @user = current_user
-    end
+  private def set_current_user
+    @user = current_user
+  end
 end
--- app/controllers/topic_assignements_controller.rb
+++ app/controllers/topic_assignements_controller.rb
@@ -1,37 +1,37 @@
 class TopicAssignementsController < ApplicationController
-    before_action :set_topicable
-    before_action :set_topic_assignement, only: %i[destroy]
+  before_action :set_topicable
+  before_action :set_topic_assignement, only: %i[destroy]
 
-    def create
-      @topic_assignement = TopicAssignement.new(
-        topic_id: params[:add_topic_id],
-        topicable: @topicable
-      )
+  def create
+    @topic_assignement = TopicAssignement.new(
+      topic_id: params[:add_topic_id],
+      topicable: @topicable
+    )
 
-      if @topic_assignement.save
-        redirect_to @topicable
-      else
-        redirect_to @topicable, alert: "Could not add topic"
-      end
+    if @topic_assignement.save
+      redirect_to @topicable
+    else
+      redirect_to @topicable, alert: "Could not add topic"
     end
+  end
 
-    def destroy
-        if @topic_assignement.destroy
-            redirect_to @topicable
-        else
-          redirect_to @topicable, alert: "Could not delete topic"
-        end
+  def destroy
+    if @topic_assignement.destroy
+      redirect_to @topicable
+    else
+      redirect_to @topicable, alert: "Could not delete topic"
     end
+  end
 
-    private def set_topicable
-        if params[:user_id]
-            @topicable = User.find_by(id: params[:user_id])
-        elsif params[:question_id]
-            @topicable = Question.find_by(id: params[:question_id])
-        end
+  private def set_topicable
+    if params[:user_id]
+      @topicable = User.find_by(id: params[:user_id])
+    elsif params[:question_id]
+      @topicable = Question.find_by(id: params[:question_id])
     end
+  end
 
-    private def set_topic_assignement
-        @topic_assignement = TopicAssignement.find_by(id: params[:id])
-    end
+  private def set_topic_assignement
+    @topic_assignement = TopicAssignement.find_by(id: params[:id])
+  end
 end
