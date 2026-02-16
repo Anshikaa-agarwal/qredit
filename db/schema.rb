c3b37e66a9da684874e5da1d7a96fcc402928f6d
Anshika Agarwal <anshika2020ag@gmail.com>
2026-02-16 15:53:55 +0530

    fixed tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_05_100000) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_11_072527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_05_100000) do
   create_table "answers", force: :cascade do |t|
     t.string "content", null: false
     t.datetime "created_at", null: false
+    t.integer "net_votes", default: 0
     t.bigint "question_id", null: false
     t.integer "status", null: false
     t.datetime "updated_at", null: false
