3467ee7e19024fb5a772909ad6f8ab4243e2dccf
Anshika Agarwal <anshika2020ag@gmail.com>
2026-02-18 16:18:59 +0530

    defined routes and validations

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     patch :publish, on: :member
   end
 
+  resources :followers, only: %i[ create destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
