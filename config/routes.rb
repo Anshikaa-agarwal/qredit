79b7d5c5701e6aa9e5869ebafe0d6ea4a39c85df
Anshika Agarwal <anshika2020ag@gmail.com>
2026-02-19 11:30:08 +0530

    view to list followers and followings

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root "home_feed#index"
 
   resources :users, only: [ :show ] do
-    get "followers", to: "users#followers"
+    get "followers_list", to: "users#followers"
     patch :update_profile, on: :member
     resources :topic_assignements, only: [ :create, :destroy ]
     resources :followers, only: %i[ create destroy ]
