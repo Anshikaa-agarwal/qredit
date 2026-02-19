0cdf333eade10794dee64c530de72dca61a45646
Anshika Agarwal <anshika2020ag@gmail.com>
2026-02-19 11:50:58 +0530

    active link effect for sidebar links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,29 @@
 module ApplicationHelper
-    def markdown(text)
-      renderer = Redcarpet::Render::HTML.new(
-        hard_wrap: true,
-        link_attributes: { rel: "nofollow", target: "_blank" }
-      )
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new(
+      hard_wrap: true,
+      link_attributes: { rel: "nofollow", target: "_blank" }
+    )
 
-      options = {
-        autolink: true,
-        tables: true,
-        fenced_code_blocks: true,
-        strikethrough: true,
-        lax_spacing: true,
-        space_after_headers: true
-      }
+    options = {
+      autolink: true,
+      tables: true,
+      fenced_code_blocks: true,
+      strikethrough: true,
+      lax_spacing: true,
+      space_after_headers: true
+    }
 
-      Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+    Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+  end
+
+  def sidebar_link_classes(path)
+    base = "px-4 py-2.5 rounded-lg transition font-medium"
+
+    if current_page?(path)
+      "#{base} bg-violet-100 text-violet-700"
+    else
+      "#{base} text-gray-700 hover:bg-gray-100"
     end
+  end
 end
