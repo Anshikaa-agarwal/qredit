03bdd112ca4ef9c812af539dd91934d42096d110
Anshika Agarwal <anshika2020ag@gmail.com>
2026-02-24 13:16:41 +0530

    resolved conversations

--- app/controllers/home_feed_controller.rb
+++ app/controllers/home_feed_controller.rb
@@ -1,20 +1,20 @@
 class HomeFeedController < ApplicationController
   def index
-    base = Question.with_attached_pdf.includes(:topics, :votes, :answers, :comments, user: [ :followers, avatar_attachment: :blob ])
+    scope = Question.with_attached_pdf.includes(:topics, :votes, :answers, :comments, user: [ :followers, avatar_attachment: :blob ]).published
     if params[:query]
       term = params[:query][:content].to_s.strip
 
       if params[:query][:search_by] == "topic"
         topic_filter = Topic.sanitize_sql_like(term) + "%"
-        @questions = base.joins(:topics)
+        @questions = scope.joins(:topics)
                         .where("topics.name ILIKE ?", topic_filter)
                         .distinct
       else
         content_filter = Question.sanitize_sql_like(term) + "%"
-        @questions = base.published.where("title ILIKE ?", content_filter)
+        @questions = scope.where("title ILIKE ?", content_filter)
       end
     else
-      @questions = base.published.order(created_at: :desc)
+      @questions = scope.order(created_at: :desc)
     end
   end
 end
