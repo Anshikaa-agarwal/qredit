dd8a772bfc05d5db1b20210f8b5c9672ec244e1d
Anshika Agarwal <anshika2020ag@gmail.com>
2026-03-02 13:25:17 +0530

    rate limit

--- app/controllers/api/topics_controller.rb
+++ app/controllers/api/topics_controller.rb
@@ -1,10 +1,13 @@
 class Api::TopicsController < ApplicationController
   QUESTION_LIMIT = 7
+  API_REQUEST_RATE_LIMIT = 50
+
+  rate_limit to: API_REQUEST_RATE_LIMIT, within: 1.hour
 
   def show
     @topic = Topic.find_by("LOWER(name) = ?", params[:name].downcase)
 
-    @questions = @topic.questions.order(created_at: :desc).limit(10).includes(:comments, :answers)
+    @questions = @topic.questions.order(created_at: :desc).limit(QUESTION_LIMIT).includes(:comments, :answers)
 
     respond_to do |format|
       format.json
