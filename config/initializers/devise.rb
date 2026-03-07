fa253056d5522ed9e84b029b6dc6c30f38dee96f
Anshika Agarwal <anshika2020ag@gmail.com>
2026-03-07 18:33:47 +0530

    skip emailing for production/fix devise credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,9 +273,9 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2,
-    Rails.application.credentials.dig(:google, :client_id),
-    Rails.application.credentials.dig(:google, :client_secret),
-  {}
+    ENV["GOOGLE_CLIENT_ID"] || Rails.application.credentials.dig(:google, :client_id),
+    ENV["GOOGLE_CLIENT_SECRET"] || Rails.application.credentials.dig(:google, :client_secret),
+    scope: "email,profile"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
