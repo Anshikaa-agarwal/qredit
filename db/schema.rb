8b03211a747b36d78068877ee53d411369071f41
Anshika Agarwal <anshika2020ag@gmail.com>
2026-03-17 14:35:51 +0530

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_10_051746) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_17_085732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -23,6 +23,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_10_051746) do
     t.datetime "updated_at", null: false
     t.index ["reportable_type", "reportable_id"], name: "index_abuses_on_reportable"
     t.index ["reporter_id"], name: "index_abuses_on_reporter_id"
+  end
+
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -128,7 +138,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_10_051746) do
 
   create_table "questions", force: :cascade do |t|
     t.boolean "abusive", default: false
-    t.string "content", null: false
+    t.text "content", default: ""
     t.datetime "created_at", null: false
     t.datetime "edited_at"
     t.datetime "posted_at"
