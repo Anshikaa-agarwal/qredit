5988d71c509aaa02b8a1a8a65252141b42e63500
Anshika Agarwal <anshika2020ag@gmail.com>
2026-03-18 17:18:20 +0530

    credit purchase receipt can be viewed by owner only

--- app/controllers/credit_purchases_controller.rb
+++ app/controllers/credit_purchases_controller.rb
@@ -1,5 +1,8 @@
 class CreditPurchasesController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @credit_purchase = CreditPurchase.find_by(id: params[:id])
+    redirect_to root_path, alert: "Not authorized" unless @credit_purchase.user == current_user
   end
 end
