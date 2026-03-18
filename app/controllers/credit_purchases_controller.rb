5de53c1d71e4479db890ac89217e3d8830795630
Anshika Agarwal <anshika2020ag@gmail.com>
2026-03-18 17:39:34 +0530

    update credit purchase params

--- app/controllers/credit_purchases_controller.rb
+++ app/controllers/credit_purchases_controller.rb
@@ -3,6 +3,11 @@ class CreditPurchasesController < ApplicationController
 
   def show
     @credit_purchase = CreditPurchase.find_by(stripe_transaction_id: params[:stripe_transaction_id])
-    redirect_to root_path, alert: "Not authorized" unless @credit_purchase.user == current_user
+    unless @credit_purchase
+      redirect_back fallback_location: root_path, alert: "Purchase receipt not found."
+      return
+    end
+
+    redirect_to root_path, alert: "Not authorized" unless @credit_purchase&.user == current_user
   end
 end
